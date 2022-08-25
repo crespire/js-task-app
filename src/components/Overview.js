6ae6a46a187094d46c1bf841bd23feeec32ffddc
crespire <li.simmon@gmail.com>
2022-08-25 15:14:43 -0400

    Update Overview component to conditionally render task or edit task and pass props

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import Task from './Task';
+import EditTask from './EditTask';
 
 class Overview extends Component {
   render() {
-    const { taskList, handleDelete } = this.props;
+    const { taskList, editMode, editID, handleDelete, toggleEdit, handleEditChange, handleEditSubmit } = this.props;
 
     return (
       <div>
         <ul>
           {taskList.map((task, i) => {
-            return <Task key={task.id} order={i+1} task={task} handleDelete={handleDelete} />;
+            if (editMode && editID === task.id) {
+              return <EditTask key={task.id} task={task} handleEditChange={handleEditChange} handleEditSubmit={handleEditSubmit}  />
+            } else {
+              return <Task key={task.id} order={i+1} task={task} handleDelete={handleDelete} toggleEdit={toggleEdit} />;
+            }
           })}
         </ul>
       </div>
