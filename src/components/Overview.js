70367fff8972ac0a35a083b6afc6c04deae74a22
crespire <li.simmon@gmail.com>
2022-08-25 15:31:25 -0400

    Update props for EditTask component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -4,16 +4,16 @@ import EditTask from './EditTask';
 
 class Overview extends Component {
   render() {
-    const { taskList, editMode, editID, handleDelete, toggleEdit, handleEditChange, handleEditSubmit } = this.props;
+    const { taskList, editMode, editID, editTask, handleDelete, toggleEdit, handleEditChange, handleEditSubmit } = this.props;
 
     return (
       <div>
         <ul>
           {taskList.map((task, i) => {
             if (editMode && editID === task.id) {
-              return <EditTask key={task.id} task={task} handleEditChange={handleEditChange} handleEditSubmit={handleEditSubmit}  />
+              return <EditTask key={task.id} task={task} editTask={editTask} handleEditChange={handleEditChange} handleEditSubmit={handleEditSubmit}  />
             } else {
-              return <Task key={task.id} order={i+1} task={task} handleDelete={handleDelete} toggleEdit={toggleEdit} />;
+              return <Task key={task.id} order={i+1} task={task} handleDelete={handleDelete} toggleEdit={toggleEdit} />
             }
           })}
         </ul>
