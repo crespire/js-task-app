25839cb3d2892813c6ffcc015d2f3f2f49f27b70
crespire <li.simmon@gmail.com>
2022-08-25 15:14:56 -0400

    Update Task component with edit button

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class Task extends Component {
   render() {
-    const { order, task, handleDelete } = this.props;
+    const { order, task, handleDelete, toggleEdit } = this.props;
 
     return (
-      <li>{order}: {task.text} <button onClick={(event) => { handleDelete(event, task.id); }}>Delete</button></li>
+      <li>
+        {order}: {task.text}
+        <button onClick={(event) => { handleDelete(event, task.id); }}>Delete</button>
+        <button onClick={(event) => { toggleEdit(event, task.id)}}>Edit</button>;
+      </li>
     );
   }
 }
